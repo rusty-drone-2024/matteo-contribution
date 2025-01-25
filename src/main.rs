eb6b5944a9509bebb307a535e3a17c9c6e07cce3
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-25 18:55:19 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ mod server;
 use crate::server::{MediaServer, TextServer};
 use client::TextMediaClient;
 use rusty_drones::RustyDrone;
-use rusty_tester::utils::Network;
 use rusty_drones_servers::MediaServer as SamMedia;
 use rusty_drones_servers::TextServer as SamText;
+use rusty_tester::utils::Network;
 //use rusty_drones_servers::ChatServer as SamChat;
 use rusty_drones_servers::Server as SamSer;
 use std::thread::sleep;
@@ -34,7 +34,7 @@ fn main() {
     ];
 
     let mut net = Network::create_and_run::<RustyDrone>(16, topology, &[0, 11, 12, 13, 14]);
-    
+
     net.create_and_run_leaf::<MediaServer>(12).unwrap();
     net.create_and_run_leaf::<SamSer<SamText>>(13).unwrap();
     net.create_and_run_leaf::<SamSer<SamMedia>>(14).unwrap();

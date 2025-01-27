ec2bc2b12e8f952c1c1c458b7a6d5847e068bb13
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-27 16:39:14 +0100

    Saner start up

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,12 @@ use crate::server::{MediaServer, TextServer};
 use client::TextMediaClient;
 use rusty_drones::RustyDrone;
 use rusty_drones_servers::MediaServer as SamMedia;
-//use rusty_drones_servers::ChatServer as SamChat;
+use rusty_drones_servers::ChatServer as SamChat;
 use rusty_drones_servers::Server as SamSer;
 use rusty_drones_servers::TextServer as SamText;
 use rusty_tester::utils::Network;
 use std::thread::sleep;
 use std::time::Duration;
-use wg_2024::controller::DroneCommand;
 
 fn main() {
     let topology = &[
@@ -34,26 +33,19 @@ fn main() {
         (10, 3),
     ];
 
-    let mut net = Network::create_and_run::<RustyDrone>(16, topology, &[0, 11, 12, 13, 14]);
-
-    for id in 1..=10u8 {
-        net.send_as_simulation_controller_to(id, DroneCommand::SetPacketDropRate(0.2))
-            .unwrap();
-    }
-
-    net.send_as_simulation_controller_to(8, DroneCommand::SetPacketDropRate(1.0))
-        .unwrap();
+    let clients = &[0, 11, 12, 13, 14, 15];
+    let mut net = Network::create_and_run::<RustyDrone>(16, topology, clients);
+    // TODO wait for other parts for PDR
 
     net.create_and_run_leaf::<MediaServer>(11).unwrap();
     net.create_and_run_leaf::<SamSer<SamText>>(12).unwrap();
     net.create_and_run_leaf::<SamSer<SamMedia>>(13).unwrap();
     net.create_and_run_leaf::<TextServer>(14).unwrap();
-    //net.create_and_run_leaf::<SamSer<SamChat>>(15).unwrap();
-
-    // TODO Need to be after as they need server to be initialized (will fix later)
+    net.create_and_run_leaf::<SamSer<SamChat>>(15).unwrap();
     net.create_and_run_leaf::<TextMediaClient>(0).unwrap();
 
     loop {
         sleep(Duration::from_secs(1));
     }
 }
+

af88a225f375930b94b9d47dd9582c883f5622d3
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-30 14:31:49 +0100

    Refactoring disassembler

--- src/backend/disassembler/waiting.rs
+++ src/backend/disassembler/waiting.rs
@@ -1,5 +1,5 @@
-use crate::backend::disassembler::Disassembler;
-use common_structs::types::{FragmentIndex, SessionId};
+use crate::backend::disassembler::{Disassembler, Split};
+use common_structs::types::Session;
 use wg_2024::network::NodeId;
 
 impl Disassembler {
@@ -10,49 +10,28 @@ impl Disassembler {
         required
     }
 
-    pub fn add_waiting(
-        &mut self,
-        session: SessionId,
-        dest: NodeId,
-        fragment_id: FragmentIndex,
-    ) -> Result<bool, String> {
+    pub fn add_session_to_wait_queue(&mut self, session: Session) -> Result<&mut Split, String> {
         let Some(split) = self.splits.get_mut(&session) else {
             return Err("Split is missing".to_string());
         };
 
-        if !split.add_waiting(fragment_id) {
-            return Ok(false);
-        }
+        let dest = split.destination();
 
         let entry = self.waiting.entry(dest).or_default();
         if entry.insert(session) {
             self.new_waiting += 1;
         }
-        Ok(true)
+        Ok(split)
     }
 
-    pub fn add_all_waiting(&mut self, session: SessionId) -> Result<bool, String> {
-        let Some(split) = self.splits.get_mut(&session) else {
-            return Err("Split is missing".to_string());
-        };
-
-        split.add_all_to_waiting();
-
-        let entry = self.waiting.entry(split.destination()).or_default();
-        if entry.insert(session) {
-            self.new_waiting += 1;
-        }
-        Ok(true)
-    }
-
-    pub fn remove_waiting_for(&mut self, dest: NodeId) {
+    pub fn ready_sessions_waiting_for(&mut self, dest: NodeId) {
         if let Some(waitings) = self.waiting.remove(&dest) {
             let entry = self.finished_waiting.entry(dest).or_default();
             entry.extend(waitings);
         }
     }
 
-    pub fn take_ready(&mut self) -> Vec<SessionId> {
+    pub fn take_ready_session(&mut self) -> Vec<Session> {
         let mut res = vec![];
         for (_, sessions) in self.finished_waiting.drain() {
             res.extend(sessions);

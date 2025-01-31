ac9e46085a738aee538a44fbc01e0bd714e8b994
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-31 16:27:35 +0100

    better gui

--- client_ui/src/view.rs
+++ client_ui/src/view.rs
@@ -1,10 +1,12 @@
 use crate::model::Message::Selected;
 use crate::model::{Message, Model};
-use iced::widget::{button, row, text, Column};
-use iced::{Element, Fill};
+use iced::widget::{button, container, row, scrollable, text, Column};
+use iced::{Element, Fill, Renderer, Theme};
+
+type El<'a> = Element<'a, Message, Theme, Renderer>;
 
 pub fn view(model: &Model) -> Element<Message> {
-    let mut res = vec![];
+    let mut res: Vec<El> = vec![];
 
     for (i, name) in model.list.iter().enumerate() {
         let txt = if i == model.selected {
@@ -16,7 +18,15 @@ pub fn view(model: &Model) -> Element<Message> {
         res.push(button(text(txt)).on_press(Selected(i)).width(Fill).into());
     }
 
-    row![Column::from_vec(res).width(200), text(&model.log),]
-        .height(Fill)
-        .into()
+    row![
+        Column::from_vec(res).width(200).spacing(2),
+        scrollable(
+            container(text(&model.log).width(Fill))
+                .padding(20)
+                .width(Fill)
+        )
+        .width(Fill),
+    ]
+    .height(Fill)
+    .into()
 }

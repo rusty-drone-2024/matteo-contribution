d081e1dc0c5071b1ab97772218a1181ef27a6daf
Matteo Di Noia <matteodinoia02@gmail.com>
2025-02-05 09:28:05 +0100

    Add image on UI

--- client_ui/src/ui/mod.rs
+++ client_ui/src/ui/mod.rs
@@ -11,8 +11,21 @@ pub struct ClientUI {
     pub addr: String,
     pub list: Vec<String>,
     pub older_task: Option<Handle>,
-    pub selected: Option<usize>,
-    pub markdown: Vec<markdown::Item>,
+    pub content_state: ContentState,
+}
+
+pub enum ContentState {
+    Empty,
+    Valid {
+        index: usize,
+        content: Vec<markdown::Item>,
+    },
+    Invalid,
+    Loading {
+        index: usize,
+        content: Option<Vec<markdown::Item>>,
+        to_load: usize, 
+    },
 }
 
 impl ClientUI {
@@ -21,15 +34,14 @@ impl ClientUI {
             addr,
             list,
             older_task: None,
-            selected: None,
-            markdown: vec![],
+            content_state: ContentState::Empty,
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    LinkClicked(markdown::Url),
+    LinkClicked(String),
     NetResponse(Option<GuiResponse>),
     Selected(usize),
     Refresh,
--- client_ui/src/ui/update/mod.rs
+++ client_ui/src/ui/update/mod.rs
@@ -1,8 +1,11 @@
-use crate::ui::{ClientUI, Message};
+use crate::ui::{ClientUI, ContentState, Message};
 use client_bridge::{GuiRequest, GuiResponse};
-use iced::widget::markdown;
 use iced::Task;
+use std::fs::write;
+use std::path::Path;
 use std::process::exit;
+use iced::widget::markdown;
+use common_structs::message::{FileWithData, Link, Media};
 
 mod communication;
 
@@ -10,10 +13,10 @@ impl ClientUI {
     pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Selected(idx) => {
-                if self.selected != Some(idx) {
-                    self.selected = Some(idx);
-
-                    let req = GuiRequest::Get(self.list[idx].to_string());
+                let link = self.select(idx);
+                
+                if let Some(link) = link {
+                    let req = GuiRequest::Get(link);
                     return self.create_task(req);
                 }
             }
@@ -22,7 +25,15 @@ impl ClientUI {
                 return self.create_task(req);
             }
             Message::LinkClicked(url) => {
-                let _ = open::that(url.as_str());
+                if url.starts_with("http") {
+                    let _ = open::that(url.as_str());
+                } else {
+                    // TODO maybe invert order check
+                    let pos = self.list.iter().position(|x| url.eq(x) );
+                    if let Some(pos) = pos {
+                        return Task::done(Message::Selected(pos));
+                    }
+                }
             }
             Message::NetResponse(resp) => {
                 let Some(resp) = resp else {
@@ -33,27 +44,84 @@ impl ClientUI {
         }
         Task::none()
     }
+    
+    fn select(&mut self, idx: usize) -> Option<Link> {
+        if let ContentState::Valid {index, ..} = self.content_state{
+            if index == idx {
+                return None;
+            }
+        }
+        
+        let link = self.list.get(idx)?;
+        self.content_state = ContentState::Loading{
+            index: idx,
+            content: None,
+            to_load: 1,
+        };
+        
+        Some(link.to_string())
+    }
 
     fn handle_net_resp(&mut self, resp: GuiResponse) -> Task<Message> {
         match resp {
             GuiResponse::Err404 => {
-                self.markdown = markdown::parse("# ERROR 404").collect();
+                self.content_state = ContentState::Invalid;
             }
-            GuiResponse::GotFile(file) => {
-                self.markdown = markdown::parse(&file.file).collect();
-            }
+            
             GuiResponse::ListOfAll(list) => {
                 let list = list.into_iter().flat_map(|(_, l)| l);
                 self.list = list.collect();
-
-                self.selected = None;
-                self.markdown.clear();
+                self.content_state = ContentState::Empty;
             }
-            GuiResponse::GotMedia(_) => {
-                eprintln!("INVALID RESPONSE {resp:?}");
+            GuiResponse::GotFile(file) => {
+                println!("FILE: {file:?}");
+                let media = self.handle_got_file(&file);
+                return Task::batch(media.into_iter().map(|link| {
+                    let req = GuiRequest::GetMedia(link);
+                    println!("req media: {req:?}");
+                    self.create_task(req)
+                }));
+            }
+            GuiResponse::GotMedia(media) => {
+                self.handle_got_media(media);
             }
         }
 
         Task::none()
+    }
+    
+    fn handle_got_file(&mut self, file: &FileWithData) -> Vec<Link>{
+        // TODO Check same
+        let ContentState::Loading {index, .. } = self.content_state else {
+            eprintln!("Received file when non waiting");
+            return vec!();
+        };
+        
+        let to_load = file.related_data.len();
+        let content = markdown::parse(&file.file).collect();
+        
+        self.content_state = if to_load > 0 {
+            let content = Some(content);
+            ContentState::Loading {index, content, to_load }
+        } else {
+            ContentState::Valid {index, content}
+        };
+        
+        file.related_data.keys().cloned().collect()
+    }
+
+    fn handle_got_media(&mut self, media: Media) {
+        let ContentState::Loading { index, content, to_load } = &mut self.content_state else {
+            return eprintln!("Received media when non waiting");
+        };
+        
+        // TODO remove hardcode (+ mkdir)
+        let _ = write(Path::new("/home/matteo/.cache/matteo_contribution_img/chicken.jpeg"), media);
+        *to_load -= 1;
+        if *to_load > 0 {
+            return;
+        }
+        
+        self.content_state = ContentState::Valid { index: *index, content: content.clone().unwrap_or_default() }
     }
 }

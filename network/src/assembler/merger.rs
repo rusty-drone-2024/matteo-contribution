84934099b8fc75125454f1f4cd54d37a36c956cd
Matteo Di Noia <matteodinoia02@gmail.com>
2025-02-06 14:23:42 +0100

    Document assembler

--- network/src/assembler/merger.rs
+++ network/src/assembler/merger.rs
@@ -1,18 +1,14 @@
 use super::Assembler;
 use common_structs::message::Message;
 use common_structs::types::Session;
-use wg_2024::packet::Fragment;
 
 impl Assembler {
-    pub fn merge_fragment(&mut self, session: Session, fragment: Fragment) -> Option<Message> {
-        if !self.add_fragment(session, fragment) {
-            return None;
-        }
-
-        self.get_full_message(session)
-    }
-
-    pub(super) fn get_full_message(&mut self, session: Session) -> Option<Message> {
+    /// Get the full message if it is ready. If it is it remove it from the pile.
+    /// As such it must be used.
+    /// # Return
+    /// The optional ready message.
+    #[must_use]
+    pub fn take_full_message(&mut self, session: Session) -> Option<Message> {
         let to_assemble = self.messages_to_assemble.get(&session)?;
 
         if (to_assemble.pieces.len() as u64) < to_assemble.pieces_number {

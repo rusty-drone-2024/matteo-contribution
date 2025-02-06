89ca3bac00e8589d62d4aa2046f059f42f75527c
Matteo Di Noia <matteodinoia02@gmail.com>
2025-02-06 13:30:36 +0100

    Document topology

--- network/src/topology/mod.rs
+++ network/src/topology/mod.rs
@@ -7,6 +7,14 @@ use petgraph::graphmap::DiGraphMap;
 use std::collections::HashMap;
 use wg_2024::network::NodeId;
 
+/// The weight of a single node, is an estimation of
+/// the drone pdr. The higher the number the higher the pdr is.
+pub type Weight = u8;
+
+/// Represent the topology using a directed graph
+/// to avoid passing throught the client and use
+/// weight to estimate the drop rate.
+/// Also keep track of `flood_id`.
 pub struct Topology {
     start_id: NodeId,
     current_flood_id: u64,
@@ -14,9 +22,10 @@ pub struct Topology {
     weights: HashMap<NodeId, u8>,
 }
 
-impl Topology {}
-
 impl Topology {
+    /// Create a new topology with the given id
+    /// that will be used as a starting point for all flood and
+    /// reachability information.
     pub fn new(this_node_id: NodeId) -> Self {
         Self {
             start_id: this_node_id,
@@ -26,6 +35,9 @@ impl Topology {
         }
     }
 
+    /// Get a new fresh `flood_id`. Doing so invalidate the old one making
+    /// it so that all `FloodResponse` received after this call that have 
+    /// older `flood_id` will be ignored.
     #[must_use]
     pub fn take_fresh_flood_id(&mut self) -> u64 {
         self.current_flood_id += 1;

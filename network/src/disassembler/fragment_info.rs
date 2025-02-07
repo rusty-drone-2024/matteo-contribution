42d66de28f9cd589ea083fd45a88c1662e232710
Matteo Di Noia <matteodinoia02@gmail.com>
2025-02-07 09:40:42 +0100

    Document disassembler

--- network/src/disassembler/fragment_info.rs
+++ network/src/disassembler/fragment_info.rs
@@ -1,26 +1,36 @@
 use std::mem::replace;
 use wg_2024::packet::Fragment;
 
+/// Include in a single struct the info about the fragment.
+/// which are the fragment itself and wheter it was acked.
 pub struct FragmentInfo {
     fragment: Fragment,
     acked: bool,
 }
 
-impl FragmentInfo {
-    pub(super) fn new(fragment: Fragment) -> Self {
+impl From<Fragment> for FragmentInfo {
+    /// Create a new `FragmentInfo` from the `fragment`. This
+    /// is considered yet to ack.
+    fn from(value: Fragment) -> Self {
         Self {
-            fragment,
+            fragment: value,
             acked: false,
         }
     }
+}
 
+impl FragmentInfo {
+    /// Ack the fragments
     /// # Return
     /// whether or not the ack was successful
     pub(super) fn ack(&mut self) -> bool {
         !replace(&mut self.acked, true)
     }
 
-    pub fn fragment(&self) -> &Fragment {
+    /// Getter method for fragment
+    /// # Return
+    /// The fragment in question
+    pub(super) fn fragment(&self) -> &Fragment {
         &self.fragment
     }
 }
--- network/src/disassembler/mod.rs
+++ network/src/disassembler/mod.rs
@@ -9,30 +9,52 @@ use common_structs::types::{FragmentIdx, Session};
 use std::collections::{HashMap, HashSet};
 use wg_2024::network::NodeId;
 
+/// Disassembler contains all message that the leaf is trying
+/// to send. Both waiting to send and waiting to ack.
 #[derive(Default)]
 pub struct Disassembler {
+    /// Messages splitted.
     splits: HashMap<Session, Split>,
+    /// The number of new waiting since last checked.
     new_waiting: usize,
+    /// The session of messages waiting for a path to a node.
     waiting: HashMap<NodeId, HashSet<Session>>,
+    /// The session of messages that found a a path to a node.
     finished_waiting: HashMap<NodeId, HashSet<Session>>,
 }
 
 impl Disassembler {
+    /// Split a `msg` in splits and store them with the given `session` and
+    /// `destination`.
+    /// If the session already existed it is replaced.
+    // TODO That is probably not good
     pub fn split(&mut self, session: Session, dest: NodeId, msg: Message) {
         let fragments = msg.into_fragments();
         let split = Split::new(dest, fragments);
         self.splits.insert(session, split);
     }
 
+    /// Getter for a single `Split` based on the `session`
+    /// # Return
+    /// A reference to the split if it is present.
     #[allow(dead_code)]
     pub fn get(&self, session: Session) -> Option<&Split> {
         self.splits.get(&session)
     }
 
+    /// Getter for a single `Split` based on the `session`
+    /// # Return
+    /// A mutable reference to the split if it is present.
     pub fn get_mut(&mut self, session: Session) -> Option<&mut Split> {
         self.splits.get_mut(&session)
     }
 
+    /// Mark a fragment decided by the `fragment_idx` of a message decided by the `session`
+    /// # Return
+    /// Wheater the ack changed the status of the fragment. Meaning it return false if
+    /// it was already ack, true otherwise.
+    /// # Error
+    /// It return error if the `session` is not present in the disassembler.
     pub fn ack(&mut self, session: Session, fragment_idx: FragmentIdx) -> Result<bool, String> {
         let split = self.splits.get_mut(&session).ok_or("No session id")?;
 
